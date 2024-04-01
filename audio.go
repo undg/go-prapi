48977eb055b05269bc489e65d5afb8b27dc3b2ef
undg <und3rdg@gmail.com>
2024-04-01 01:09:01 +0100

    add toggle mute, simplify names

--- audio.go
+++ audio.go
@@ -7,18 +7,68 @@ import (
 	"mrogalski.eu/go/pulseaudio"
 )
 
-func getVol() string {
+// don't forget to closeClient()
+func openClient() pulseaudio.Client {
 	client, err := pulseaudio.NewClient()
 	if err != nil {
 		panic(err)
 	}
-	vol, err := client.Volume()
+
+	return *client
+}
+
+func closeClient(client pulseaudio.Client) {
+	defer client.Close()
+}
+
+func getVol() float32 {
+	client := openClient()
+
+	volume, err := client.Volume()
 	if err != nil {
-		log.Println(err)
+		log.Println("ERROR getVol client.Volume", err)
 	}
 
-	defer client.Close()
-	// Use `client` to interact with PulseAudio
+	closeClient(client)
+	return volume
+}
 
-	return fmt.Sprint(vol)
+func setVol(vol float32) float32 {
+	client := openClient()
+
+	err := client.SetVolume(vol)
+	if err != nil {
+		log.Println("ERROR setVol client.SetVolume", err)
+	}
+
+	currVol, err := client.Volume()
+	if err != nil {
+		log.Println("ERROR setVol client.Volume", err)
+	}
+
+	closeClient(client)
+	return currVol
+}
+
+func toggleMute() float32 {
+	client := openClient()
+
+	b, err := client.ToggleMute()
+	if err != nil {
+		log.Println("ERROR setVol client.SetVolume", err)
+	}
+
+	if b {
+		fmt.Println("mute on")
+	} else {
+		fmt.Println("mute off")
+	}
+
+	currVol, err := client.Volume()
+	if err != nil {
+		log.Println("ERROR setVol client.Volume", err)
+	}
+
+	closeClient(client)
+	return currVol
 }
