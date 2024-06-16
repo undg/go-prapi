5d837e992a2f52e290624735b5a45d1ca12b1efc
undg <und3rdg@gmail.com>
2024-06-16 02:09:04 +0100

    bash syntax is lol

--- bump.sh
+++ bump.sh
@@ -6,6 +6,6 @@ LATEST_SHA=$(git rev-parse origin/master)
 NEW_TAG=$(semver -c -i $RELEASE_TYPE $LATEST_TAG)
 
 # echo $LATEST_TAG "v$NEW_TAG" $LATEST_SHA
-git tag "v$(NEW_TAG)" $(LATEST_SHA)
-git push origin "v$(NEW_TAG)"
+git tag "v$NEW_TAG" $LATEST_SHA
+git push origin "v$NEW_TAG"
 
