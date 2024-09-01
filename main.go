12e834f7f7d67dbf444c9378a5968510fe2b16d7
undg <und3rdg@gmail.com>
2024-09-01 19:52:38 +0100

    rewrite router to serve SPA by the same server

--- main.go
+++ main.go
@@ -8,18 +8,40 @@ import (
 
 const PORT = ":8448"
 
-func setupRoutes() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/ws", wsEndpoint)
+func setupRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("/api/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/api/":
+			apiDoc(w, r)
+		case "/api/v1/ws":
+			wsEndpoint(w, r)
+		default:
+			http.NotFound(w, r)
+		}
+	})
+	fs := http.FileServer(http.Dir("./build-fe"))
+	mux.Handle("/", fs)
+}
+
+func serveWeb() {
+	fmt.Println("Starting WEB server")
+	fs := http.FileServer(http.Dir("./build-fe"))
+	http.Handle("/", fs)
+
+	log.Println("Listening on http://localhost:", PORT)
+	err := http.ListenAndServe(PORT, nil)
+	if err != nil {
+		log.Fatal("ERROR ", err)
+	}
 }
 
 func main() {
-	fmt.Println("Starting server")
+	fmt.Println("Listening on http://localhost" + PORT)
+	mux := http.NewServeMux()
+	setupRoutes(mux)
 
-	setupRoutes()
-
-	err := http.ListenAndServe(PORT, nil)
+	err := http.ListenAndServe(PORT, mux)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("ERROR ", err)
 	}
 }
