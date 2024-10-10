371266f7fdbbc73d3c2510128a47887bb419a77b
undg <und3rdg@gmail.com>
2024-10-10 13:13:27 +0100

    better error and log messages

--- ws/handlers.go
+++ ws/handlers.go
@@ -12,23 +12,24 @@ import (
 )
 
 func handleSetSinkVolume(msg json.Message, res json.Response) {
-	logPrefix := "Error [handleSetSinkVolume()]:"
+	errPrefix := "ERROR [handleSetSinkVolume()]"
+
 	if sinkInfo, ok := msg.Payload.(map[string]interface{}); ok {
 		name, ok := sinkInfo["name"].(string)
 		if !ok {
-			log.Printf("%s sinkInfo['name'].(string) not ok\n", logPrefix)
+			log.Printf("%s sinkInfo['name'].(string) NOT OK\n", errPrefix)
 		}
 
 		volume, ok := sinkInfo["volume"].(string)
 		if !ok {
-			log.Printf("%s sinkInfo['volume'].(string) not ok\n", logPrefix)
+			log.Printf("%s sinkInfo['volume'].(string) NOT OK\n", errPrefix)
 		}
 
 		pactl.SetSinkVolume(name, volume)
 
 		status, err := pactl.GetStatus()
 		if err != nil {
-			log.Printf("%s pactl.GetStatus(), err: %s\n", logPrefix, err)
+			log.Printf("%s pactl.GetStatus(), err: %s\n", errPrefix, err)
 		}
 
 		res.Payload = status
@@ -39,23 +40,24 @@ func handleSetSinkVolume(msg json.Message, res json.Response) {
 }
 
 func handleSetSinkMuted(msg json.Message, res json.Response) {
-	logPrefix := "Error [handleSetSinkMuted()]:"
+	errPrefix := "ERROR [handleSetSinkMuted()]:"
+
 	if sinkInfo, ok := msg.Payload.(map[string]interface{}); ok {
 		name, ok := sinkInfo["name"].(string)
 		if !ok {
-			log.Printf("%s sinkInfo['name'].(string) not ok\n", logPrefix)
+			log.Printf("%s sinkInfo['name'].(string) NOT OK\n", errPrefix)
 		}
 
 		muted, ok := sinkInfo["muted"].(string)
 		if !ok {
-			log.Printf("%s sinkInfo['muted'].(bool) not ok\n", logPrefix)
+			log.Printf("%s sinkInfo['muted'].(bool) NOT OK\n", errPrefix)
 		}
 
 		pactl.SetSinkMuted(name, muted)
 
 		volStatus, err := pactl.GetStatus()
 		if err != nil {
-			log.Printf("%s pactl.GetStatus(), err: %s\n", logPrefix, err)
+			log.Printf("%s pactl.GetStatus(), err: %s\n", errPrefix, err)
 		}
 
 		res.Payload = volStatus
@@ -66,25 +68,25 @@ func handleSetSinkMuted(msg json.Message, res json.Response) {
 }
 
 func handleSetSinkInputVolume(msg json.Message, res json.Response) {
-	logPrefix := "Error [handleSetSinkInputVolume()]:"
+	errPrefix := "ERROR [handleSetSinkInputVolume()]:"
+
 	if sinkInputInfo, ok := msg.Payload.(map[string]interface{}); ok {
 		id, ok := sinkInputInfo["id"].(float64)
-			log.Printf("\n\n\n%v\n\n\n\n", msg.Payload)
 		if !ok {
-			log.Printf("%s sinkInfo['id'].(int) not ok\n", logPrefix)
+			log.Printf("%s sinkInfo['id'].(float64) NOT OK\n", errPrefix)
 		}
 
 		volume, ok := sinkInputInfo["volume"].(string)
 		if !ok {
-			log.Printf("%s sinkInfo['volume'].(string) not ok\n", logPrefix)
+			log.Printf("%s sinkInfo['volume'].(string) NOT OK\n", errPrefix)
 		}
 
-		pactl.SetSinkInputVolume(fmt.Sprintf("%.0f",id), volume)
+		pactl.SetSinkInputVolume(fmt.Sprintf("%.0f", id), volume)
 
 		status, err := pactl.GetStatus()
 
 		if err != nil {
-			log.Printf("%s pactl.GetStatus(), err: %s\n", logPrefix, err)
+			log.Printf("%s pactl.GetStatus(), err: %s\n", errPrefix, err)
 		}
 		res.Payload = status
 	} else {
@@ -94,23 +96,24 @@ func handleSetSinkInputVolume(msg json.Message, res json.Response) {
 }
 
 func handleSetSinkInputMuted(msg json.Message, res json.Response) {
-	logPrefix := "Error [handleSetSinkInputMuted()]:"
+	errPrefix := "ERROR [handleSetSinkInputMuted()]:"
+
 	if sinkInputInfo, ok := msg.Payload.(map[string]interface{}); ok {
 		name, ok := sinkInputInfo["id"].(string)
 		if !ok {
-			log.Printf("%s sinkInfo['id'].(string) not ok\n", logPrefix)
+			log.Printf("%s sinkInfo['id'].(string) NOT OK\n", errPrefix)
 		}
 
 		muted, ok := sinkInputInfo["muted"].(string)
 		if !ok {
-			log.Printf("%s sinkInfo['muted'].(bool) not ok\n", logPrefix)
+			log.Printf("%s sinkInfo['muted'].(bool) NOT OK\n", errPrefix)
 		}
 
 		pactl.SetSinkMuted(name, muted)
 
 		volStatus, err := pactl.GetStatus()
 		if err != nil {
-			log.Printf("%s pactl.GetStatus(), err: %s\n", logPrefix, err)
+			log.Printf("%s pactl.GetStatus(), err: %s\n", errPrefix, err)
 		}
 
 		res.Payload = volStatus
@@ -121,64 +124,87 @@ func handleSetSinkInputMuted(msg json.Message, res json.Response) {
 }
 
 func handleGetCards(res *json.Response) {
+	errPrefix := "ERROR [handleGetCards()]"
+	debugPrefix := "DEBUG [handleGetCards()]"
+
 	cards, err := audiodeprecated.GetCards()
 	if err != nil {
-		log.Printf("ERROR readerJson GetCards %s\n", err)
+		log.Printf("%s audiodeprecated.GetCards(): %s\n", errPrefix, err)
 		res.Error = "ERROR can't get cards information from the system"
 		res.Status = json.StatusError
 	}
+
 	b, err := j.Marshal(cards)
 	if err != nil {
-		log.Printf("ERROR readerJson j.Marshal %s\n", err)
+		log.Printf("%s j.Marshal(cards): %s\n", errPrefix, err)
 		res.Error = "ERROR can't pull cards information"
 		res.Status = json.StatusError
 	}
+
 	res.Payload = string(b)
 	if utils.DEBUG {
-		log.Printf("handleGetCards %s\n", res.Payload)
+		log.Printf("%s handleGetCards(res).Action: %s\n", debugPrefix, res.Action)
+		log.Printf("%s handleGetCards(res).Payload: %s\n", debugPrefix, res.Payload)
 	}
 }
 
 func handleGetOutputs(res *json.Response) {
+	errPrefix := "ERROR [handleGetOutputs()]"
+	debugPrefix := "DEBUG [handleGetOutputs()]"
+
 	outputs, err := audiodeprecated.GetOutputs()
 	if err != nil {
-		log.Printf("ERROR readerJson getOutputs %s\n", err)
+		log.Printf("%s audiodeprecated.GetOutputs(): %s\n", errPrefix, err)
+
 		res.Error = "ERROR can't get outputs information from the system"
 		res.Status = json.StatusError
 	}
+
 	b, err := j.Marshal(outputs)
 	if err != nil {
-		log.Printf("ERROR readerJson j.Marshal %s\n", err)
+		log.Printf("%s j.Marshal(): %s\n", errPrefix, err)
+
 		res.Error = "ERROR can't pull outputs information"
 		res.Status = json.StatusError
 	}
+
 	res.Payload = string(b)
+
 	if utils.DEBUG {
-		log.Printf("handleGetOutputs %s\n", res.Payload)
+		log.Printf("%s res.Payload: %s\n",debugPrefix, res.Payload)
 	}
 }
 
 func handleGetSchema(res *json.Response) {
+	debugPrefix := "DEBUG [handleGetSchema()]"
 	schema := json.GetSchemaJSON()
+
 	res.Payload = schema
 	if utils.DEBUG {
-		log.Printf("handleGetSchema %s\n", res.Payload)
+		log.Printf("%s res.Action: %s\n",debugPrefix, res.Action)
+		log.Printf("%s res.Payload: %s\n",debugPrefix, res.Payload)
 	}
 }
 
 func handleServerLog(msg *json.Message, res *json.Response) {
-	log.Println("")
+	errPrefix := "ERROR [handleServerLog()]"
+
+	fmt.Printf("\n-->\n")
+
 	if msg != nil {
 		msgBytes, err := j.MarshalIndent(msg, "", "	")
 		if err != nil {
-			log.Printf("ERROR serverLog j.MarshalIndent %s\n", err)
+			log.Printf("%s j.MarshalIndent(): %s\n", errPrefix, err)
 		}
-		log.Printf("Client message: %s\n", string(msgBytes))
+		log.Printf("CLIENT message: %s\n", string(msgBytes))
 	}
 
-	resBytes, err := res.MarshalJSON()
+	resBytes, err := j.MarshalIndent(res, "", "	")
 	if err != nil {
-		log.Printf("ERROR serverLog res.MarshalJson %s\n", err)
+		log.Printf("%s serverLog res.MarshalJson %s\n", errPrefix, err)
 	}
-	fmt.Printf("LOG response: %s\n", string(resBytes))
+
+	log.Printf("SERVER response: %s\n", string(resBytes))
+
+	fmt.Printf(">--\n")
 }
